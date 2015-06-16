ad58ed724c18f35478603b03fa2f5bac0e2749a5
Jason Owen <jowen@tripwire.com>
2015-06-16 13:29:07 -0700

    require stdin to use scripts
    Since renaming files in c734a3b2c9ebd0cf779e0ec3448a58f417c54085, the
    attendance and speaker list scripts haven't worked. Modify them to
    operate on standard input, so that you can choose which file to use.

--- lunch_and_learn_speakers.sh
+++ lunch_and_learn_speakers.sh
@@ -1,5 +1,4 @@
 #/usr/bin/env bash
 
 ruby -ryaml -rjson -e 'puts JSON.pretty_generate(YAML.load(ARGF))' \
-  < LunchAndLearns.yml \
   | jq "def flatten: reduce .[] as \$i ([]; if \$i | type == \"array\" then . + (\$i | flatten) else . + [\$i] end); [.[].videos[].speaker]|flatten|unique"
